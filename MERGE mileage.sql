33589001d2a9008849eb7bf6dc3648747c26f415
eashurak <eashurak@yahoo.com>
2015-10-16 11:22:44 -0700

    Merge example

---
+++ MERGE mileage.sql
@@ -0,0 +1,7 @@
+﻿
+  MERGE tblMileage as t
+USING [tblMileageImport] as s
+ON (T.nhid = s.NHID AND t.INTID = s.[IntID])
+WHEN MATCHED THEN
+WHEN NOT MATCHED THEN
+INSERT   (NHID,IntID,Mileage) VALUES (s.nhid,s.intid, s.[Mileage]);
